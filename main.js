bd2774787c72a94142e9f337194988b7ccb1312d
Davronnn <kifleader@gmail.com>
2022-12-10 19:02:29 +0500

    as

---
+++ main.js
@@ -0,0 +1,8 @@
+// Imtihon me’zon
+// deadline: 10.12.2022 18:00
+// Masalalar:
+// 1. funksiyda kiritilgan arrayni ichidagi musbat raqam soni va manfiy elementlar yig’indisini array ko’rinishida chiqaring
+// 2. Palindrome songa tekshiruvchi funksiya (121)
+// 3. kabisa yiliga tekshiruvchi funksiya 
+// Kabisa yil (arabcha: toʻliq) — oddiy yildan bir kun (fevral oyida) ortiq boʻlgan kalendar yil. Bunda 4 ga qoldiqsiz boʻlinadigan yil Kabisa yildir (faqat ikkita nol bilan tugab, 400 ga boʻlinmaydigan yil bundan mustasno. Masalan: 1800 , 1900, 2100-yillar Kabisa yil emas)
+// 4.Funksiyaga ixtiyoriy sonlardan iborat array kiritiladi shu arraydagi manfiy sonlarni alohida qilib ularni array ko’rinishida qaytaring.
